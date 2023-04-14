a76a4de08c36f46c0134d7ad708705bc7321562e
DualSeven <samuellpasa@gmail.com>
2023-04-14 13:31:42 -0300

    intro- teste
    introduccion prueba para  verificar la instalacion correcta de extensiones python, format pylint, e configuraciones escenciales para el trabajo.
    
    testes de introdução para verificar a correta instalação de extensões python, formato pylint e configurações essenciais para o trabalho.
    
    introduction tests to verify the correct installation of python extensions, format pylint, and essential configurations for the job.

---
+++ proyecto-python/format.py
@@ -0,0 +1,9 @@
+""""intro a python"""
+
+Cochino = 'feliz'
+
+a = 12
+b = 13
+c = 13
+
+print(Cochino, a, b, c)  # adsad
---
+++ proyecto-python/intro.py
@@ -0,0 +1,8 @@
+"""Introduccion a Python"""
+
+print('hola mundo')
+print('la we'*4)
+
+NUM = 3+2
+PRINT=NUM
+
